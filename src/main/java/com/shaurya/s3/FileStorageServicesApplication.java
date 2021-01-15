5b826b5d4e49ff9af7be6f288168ba81ba4a1f47
Lord Vader <72856051+lord-vader-saber@users.noreply.github.com>
2021-01-15 14:21:09 +0530

    Initial commit

---
+++ src/main/java/com/shaurya/s3/FileStorageServicesApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaurya/s3/FileStorageServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
